5ed4b2d0b3b3e0682b242101f0edad6db8df5693
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-25 18:41:32 +0300

    step 3

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { Description } from "./components/Description/Description";
 import { Options } from "./components/Options/Options";
 import { Feedback } from "./components/Feedback/Feedback";
+import { Notification } from "./components/Notification/Notification";
 
 function App() {
   const [feedback, setFeedback] = useState({
@@ -19,11 +20,17 @@ function App() {
       [feedbackType]: prevState[feedbackType] + 1,
     }));
   };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   return (
     <>
       <Description></Description>
       <Options updateFeedback={updateFeedback}></Options>
-      <Feedback feedback={feedback}></Feedback>
+      {totalFeedback > 0 ? (
+        <Feedback feedback={feedback} />
+      ) : (
+        <Notification message="No feedback given yet." />
+      )}
     </>
   );
 }
