e4318b9c595d2ae006651364e2ed758971ad8940
Ihor_Haraniuk <igorgaranyuk133@gmail.com>
2024-09-26 12:07:55 +0300

    add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { Description } from "./components/Description/Description";
 import { Options } from "./components/Options/Options";
 import { Feedback } from "./components/Feedback/Feedback";
 import { Notification } from "./components/Notification/Notification";
+import { useEffect } from "react";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
   });
 
   const updateFeedback = (feedbackType) => {
@@ -33,6 +35,10 @@ function App() {
 
   const positiveFeedbackPercentage =
     totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
   return (
     <>
       <Description></Description>
